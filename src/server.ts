52691e39e27800232d5dc9131a87f3ed2eb264e8
ZahidHR99 <zahidhr99@gmail.com>
2024-02-06 08:14:33 +0600

    project setup

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function bootstrap() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log("DB Connected");
+
+    app.listen(config.port, () => {
+      console.log(`app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log("DB Connection fail", err);
+  }
+}
+
+bootstrap();
