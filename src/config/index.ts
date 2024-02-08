2d2c19130f36819b80f790d373fd96fccd83a268
ZahidHR99 <zahidhr99@gmail.com>
2024-02-08 08:40:56 +0600

    create model interface

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,9 @@
-import dotenv from "dotenv";
-import Path from "path";
-dotenv.config({ path: Path.join(process.cwd(), ".env") });
+import dotenv from 'dotenv'
+import Path from 'path'
+dotenv.config({ path: Path.join(process.cwd(), '.env') })
 
 export default {
   port: process.env.PORT,
   database_url: process.env.DB_URL,
-};
+  default_student_pass: process.env.DEFAULT_STUDENT_PASSWORD,
+}
