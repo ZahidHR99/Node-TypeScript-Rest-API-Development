c3c0742aa93951284f2b9e090f2f553b1ea2d85d
ZahidHR99 <zahidhr99@gmail.com>
2024-02-08 07:24:51 +0600

    commit

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
-import express, { Application } from "express";
-const app: Application = express();
-const port = 3000;
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(cors())
 
-export default app;
+//parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Working Successfully')
+})
+
+export default app
